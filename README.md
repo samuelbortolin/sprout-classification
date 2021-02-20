cd9a0282c600a8f45e37b18b0744839404f80994
Samuel Bortolin <samuel.bortolin@u-hopper.com>
2021-02-20 19:16:47 +0100

    Code refactor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sprout-classification
+
 Project of signal, image and video @ Università degli studi di Trento for sprout classification
+
+## Setup and configuration
+
+### Required Python Packages
+
+Required Python packages can be installed using the command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## main.py
+
+Change the path with the image to analyze and run the program to extract the relevant edges of the image.
--- src/main.py
+++ src/main.py
@@ -1,55 +1,51 @@
+from __future__ import absolute_import, annotations
+
 import cv2 as cv
-import numpy
+import numpy as np
 
 
-max_lowThreshold = 100
-window_name = 'Edge Map'
-title_trackbar = 'Min Threshold:'
-ratio = 3
-kernel_size = 3
-img = cv.imread("../media/IMG_20190327_082544_01.JPG")
-print(len(img[0]) * len(img))
-scale = (len(img[0]) * len(img)/100000)**(1/2)
-print(scale)
-img = cv.resize(img, (0, 0), fx=(1/scale), fy=(1/scale))
-greyscale_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(greyscale_img,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+ratio = 3  # we can try to set a high threshold instead of using this ratio
+kernel_size = 3  # we can try to understand what is that
+image = cv.imread("../media/image.jpg")
+# print(len(img[0]) * len(img))
+scale = (len(image[0]) * len(image) / 100000) ** (1 / 2)
+# print(scale)
+
+# rescale image all to the same standard number of pixels
+image = cv.resize(image, (0, 0), fx=(1 / scale), fy=(1 / scale))
+greyscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 
-
-def CannyThreshold(val):
-    low_threshold = val
-    img_blur = cv.blur(greyscale_img, (3, 3))
-    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
+def canny_threshold(low_threshold: int) -> int:
+    blurred_image = cv.blur(greyscale_image, (3, 3))
+    detected_edges = cv.Canny(blurred_image, low_threshold, low_threshold * ratio, kernel_size)
     mask = detected_edges != 0
-    dst = img * (mask[:, :, None].astype(img.dtype))
-    return numpy.sum(mask)
+    return np.ma.sum(mask)
 
-def CannyThresholdWithImage(val):
-    low_threshold = val
-    img_blur = cv.blur(greyscale_img, (3, 3))
-    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
+def canny_threshold_with_image(low_threshold: int) -> None:
+    blurred_image = cv.blur(greyscale_image, (3, 3))
+    detected_edges = cv.Canny(blurred_image, low_threshold, low_threshold * ratio, kernel_size)
     mask = detected_edges != 0
-    dst = img * (mask[:, :, None].astype(img.dtype))
-    cv.imshow("img", dst)
+    edges_on_image = image * (mask[:, :, None].astype(image.dtype))
+    cv.imshow("relevant edges of the image", edges_on_image)
 
 
 if __name__ == "__main__":
-    previousTreshold = CannyThreshold(25)
-    previousDelta = CannyThreshold(24) - previousTreshold
-    pickedValue = 0
+    previous_threshold = canny_threshold(25)
+    previous_delta = canny_threshold(24) - previous_threshold
+    picked_value = 0
     for i in range(26, 1000):
-        newTreshold = CannyThreshold(i)
-        delta = previousTreshold - newTreshold
-        previousTreshold = newTreshold
-        print(i, ": ", delta)
-        if (delta + previousDelta) < 50:
-            pickedValue = i
-            print(CannyThreshold(i))
+        new_threshold = canny_threshold(i)
+        delta = previous_threshold - new_threshold
+        previous_threshold = new_threshold
+        # print(i, ": ", delta)
+        if (delta + previous_delta) < 50:
+            picked_value = i
+            # print(canny_threshold(i))
             break
-        previousDelta = delta
-    CannyThresholdWithImage(pickedValue)
-    cv.imshow("original", img)
+        previous_delta = delta
+
+    # show the original and the resulting image
+    cv.imshow("original image", image)
+    canny_threshold_with_image(picked_value)
     cv.waitKey()
-
-
