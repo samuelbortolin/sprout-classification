b7b352141d1c331590515d156faffac18eea6093
FunnyStones <alessandro.grassi1998@gmail.com>
2021-03-05 19:00:34 +0100

    remove percentile from sobel

--- src/canny.py
+++ src/canny.py
@@ -2,11 +2,13 @@ from __future__ import absolute_import, annotations
 
 import cv2 as cv
 import numpy as np
+from copy import deepcopy
+from typing import List
 
 from play_with_HSV import rescale_image, get_hsv_mask
 
 
-image_path = "../media/IMG_20200324 LC01a BASSANI.jpg"  # in the future we can set the path as argument or env var
+image_path = "../media/20200409_102627_PV01_lucchelli.jpg"  # in the future we can set the path as argument or env var
 
 ratio = 3  # we can try to set a high threshold instead of using this ratio
 kernel_size = 3  # we can try to understand what is that
@@ -16,6 +18,14 @@ delta = 0
 depth = cv.CV_16S
 n_derivative = 2
 
+
+def grab_contours(contour_tuple: tuple) -> List[np.ndarray]:
+    # in OpenCV v2.4, v4-official
+    if len(contour_tuple) == 2:
+        return contour_tuple[0]
+    # in OpenCV v3
+    elif len(contour_tuple) == 3:
+        return contour_tuple[1]
 
 def canny_threshold(greyscale_image: np.ndarray, low_threshold: int) -> np.ndarray:
     # apply canny and return the edges
@@ -47,17 +57,14 @@ if __name__ == "__main__":
     image = cv.imread(image_path)
     image = rescale_image(image)
     greyscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    color = input("What do you want to analyze? Write w for white/flowers, g for green/leaves or b for brown/branches: ")
-    hsv_filtered_image = get_hsv_mask(image, cv.cvtColor(image, cv.COLOR_BGR2HSV), color)
-    blurred_image = cv.GaussianBlur(hsv_filtered_image, (3, 3), 0)
+
+    blurred_image = cv.GaussianBlur(image, (3, 3), 0)
     greyscale_blurred_image = cv.cvtColor(blurred_image, cv.COLOR_BGR2GRAY)
 
     gradient_x = cv.Sobel(greyscale_blurred_image, depth, n_derivative, 0, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
     gradient_y = cv.Sobel(greyscale_blurred_image, depth, 0, n_derivative, ksize=3, scale=scale, delta=delta, borderType=cv.BORDER_DEFAULT)
     abs_gradient_x = cv.convertScaleAbs(gradient_x)
     abs_gradient_y = cv.convertScaleAbs(gradient_y)
-    cv.imshow("abs_grad_x", abs_gradient_x)
-    cv.imshow("abs_grad_y", abs_gradient_y)
     gradient_image = cv.addWeighted(abs_gradient_x, 0.5, abs_gradient_y, 0.5, 0)
     cv.imshow("sobel gradient image", gradient_image)
     kernel = np.ones((3, 3), np.uint8)
@@ -65,12 +72,19 @@ if __name__ == "__main__":
     cv.imshow("closing after sobel", closing)
     open_after_closing = cv.morphologyEx(closing, cv.MORPH_OPEN, kernel)
     cv.imshow("open after closing", open_after_closing)
-    open_after_sobel = cv.morphologyEx(gradient_image, cv.MORPH_OPEN, kernel)
-    cv.imshow("open after sobel", open_after_sobel)
-    close_after_opening = cv.morphologyEx(open_after_sobel, cv.MORPH_CLOSE, kernel)
-    cv.imshow("close after opening", close_after_opening)
-    # since the edges aren't always really clear it is more effective to do closing first and then opening, otherwise we end up with disconnected edges
 
+    positiveValuesArray = []
+    for i, itemi in enumerate(open_after_closing):
+        for j, itemj in enumerate(itemi):
+            if itemj > 0:
+                positiveValuesArray.append(itemj)
+    percentile = np.percentile(positiveValuesArray, 85)
+
+    for i, itemi in enumerate(open_after_closing):
+        for j, itemj in enumerate(itemi):
+            if itemj < percentile:
+                open_after_closing[i, j] = 0
+    cv.imshow("percentile removed", open_after_closing)
     # try a edge detector approach
     previous_threshold = canny_threshold_mask(greyscale_image, 25)
     previous_delta = canny_threshold_mask(greyscale_image, 24) - previous_threshold
@@ -88,5 +102,35 @@ if __name__ == "__main__":
     cv.imshow("original image", image)
     edges_on_image = canny_threshold_with_image(greyscale_image, picked_threshold, image)
     cv.imshow("relevant edges of the image", edges_on_image)
+    greyscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    edged = canny_threshold(greyscale_image, picked_threshold)
+    print(type(edged[0, 0]))
+    bitwise_and = np.zeros((len(image), len(image[0])))
+    for i, itemi in enumerate(edges_on_image):
+        for j, itemj in enumerate(itemi):
+            if itemj.any() > 0 and open_after_closing[i, j] > 0:
+                bitwise_and[i, j] = int(255)
+            else:
+                bitwise_and[i, j] = int(0)
+    print(type(bitwise_and[0, 0]))
+    bitwise_and = bitwise_and.astype(np.uint8)
+    print(type(bitwise_and[0, 0]))
+    cv.imshow("bitwise", bitwise_and)
+    contour_tuple = cv.findContours(bitwise_and.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = grab_contours(contour_tuple)
+    contour_image = bitwise_and.copy()
+    area = 0
+
+    for c in contours:
+        area += cv.contourArea(c)
+        cv.drawContours(contour_image, [c], 0, (100, 5, 10), 10)
+
+    image_from_contours = deepcopy(image)
+    for i, item_i in enumerate(contour_image):
+        for j, item_j in enumerate(item_i):
+            if item_j.all() == 0:
+                image_from_contours[i][j] = (0, 0, 0)
+    cv.imshow("image_from_contours", image_from_contours)
+
     cv.waitKey()
     cv.destroyAllWindows()
--- src/main.py
+++ src/main.py
@@ -6,36 +6,70 @@ from typing import List
 import cv2 as cv
 import numpy as np
 
-from canny import canny_threshold_mask, canny_threshold_with_image, canny_threshold
-from play_with_HSV import rescale_image, get_hsv_mask
+from play_with_HSV import apply_mask, rescale_image
 
 
-image_path = "../media/ROBI_BO01_BBCH12_sacchetti.jpg"  # in the future we can set the path as argument or env var
+ratio = 3  # we can try to set a high threshold instead of using this ratio
+kernel_size = 3  # we can try to understand what is that
+
+image = cv.imread("../media/20200402_104001.jpg")  # in the future we can set the path as argument or env var
+image = rescale_image(image)
+greyscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+color = input("What do you want to analyze? Write w for white/flowers, g for green/leaves or b for brown/branches\n")
+
+
+def get_hsv_mask(original_image: np.ndarray, frame_hsv: np.ndarray) -> None:
+    if color == "w":
+        lower_bound = np.array([15, 0, 100])
+        upper_bound = np.array([35, 40, 255])
+    elif color == "g":  # values to be estimated using the color_picker and then tested with play_with_HSV
+        lower_bound = np.array([35, 100, 100])
+        upper_bound = np.array([80, 255, 255])
+    elif color == "b":  # values to be estimated using the color_picker and then tested with play_with_HSV
+        lower_bound = np.array([30, 0, 0])
+        upper_bound = np.array([50, 75, 200])
+    else:
+        lower_bound = np.array([0, 0, 0])
+        upper_bound = np.array([180, 255, 255])
+    apply_mask(deepcopy(original_image), frame_hsv, lower_bound, upper_bound)
 
 
 def grab_contours(contour_tuple: tuple) -> List[np.ndarray]:
-    # grab contours
-
-    # for OpenCV v2.4, v4-official
+    # in OpenCV v2.4, v4-official
     if len(contour_tuple) == 2:
         return contour_tuple[0]
-    # for OpenCV v3
+    # in OpenCV v3
     elif len(contour_tuple) == 3:
         return contour_tuple[1]
 
 
+def canny_threshold(low_threshold: int) -> np.ndarray:
+    blurred_image = cv.blur(greyscale_image, (3, 3))
+    return cv.Canny(blurred_image, low_threshold, low_threshold * ratio, kernel_size)
+
+
+def canny_threshold_mask(low_threshold: int) -> int:
+    blurred_image = cv.blur(greyscale_image, (3, 3))
+    detected_edges = cv.Canny(blurred_image, low_threshold, low_threshold * ratio, kernel_size)
+    mask = detected_edges != 0
+    return np.ma.sum(mask)
+
+
+def canny_threshold_with_image(low_threshold: int) -> np.ndarray:
+    blurred_image = cv.blur(greyscale_image, (3, 3))
+    detected_edges = cv.Canny(blurred_image, low_threshold, low_threshold * ratio, kernel_size)
+    mask = detected_edges != 0
+    return image * (mask[:, :, None].astype(image.dtype))
+
+
 if __name__ == "__main__":
-
-    image = cv.imread(image_path)
-    image = rescale_image(image)
-    greyscale_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
     # try a edge detector approach
-    previous_threshold = canny_threshold_mask(greyscale_image, 25)
-    previous_delta = canny_threshold_mask(greyscale_image, 24) - previous_threshold
+    previous_threshold = canny_threshold_mask(25)
+    previous_delta = canny_threshold_mask(24) - previous_threshold
     picked_threshold = 0
     for i in range(26, 1000):
-        new_threshold = canny_threshold_mask(greyscale_image, i)
+        new_threshold = canny_threshold_mask(i)
         delta = previous_threshold - new_threshold
         previous_threshold = new_threshold
         if (delta + previous_delta) < 50:
@@ -45,11 +79,11 @@ if __name__ == "__main__":
 
     # show the original and the resulting image
     cv.imshow("original image", image)
-    edges_on_image = canny_threshold_with_image(greyscale_image, picked_threshold, image)
+    edges_on_image = canny_threshold_with_image(picked_threshold)
     cv.imshow("relevant edges of the image", edges_on_image)
 
     # try to connect open edges with contours
-    edged = canny_threshold(greyscale_image, picked_threshold)
+    edged = canny_threshold(picked_threshold)
     contour_tuple = cv.findContours(edged.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     contours = grab_contours(contour_tuple)
     contour_image = edged.copy()
@@ -66,34 +100,16 @@ if __name__ == "__main__":
                 image_from_contours[i][j] = (0, 0, 0)
     cv.imshow("area", image_from_contours)
 
-    # try color after edges
-    color = input("What do you want to analyze? Write w for white/flowers, g for green/leaves or b for brown/branches: ")
-    hsv_filtered_image = get_hsv_mask(image, cv.cvtColor(image, cv.COLOR_BGR2HSV), color)
-    cv.imshow("hsv_filtered_image", hsv_filtered_image)
-    edges_after_hsv = canny_threshold_with_image(cv.cvtColor(hsv_filtered_image, cv.COLOR_BGR2GRAY), picked_threshold, hsv_filtered_image)
-    cv.imshow("relevant edges of the image after hsv", edges_after_hsv)
-
-    # show original image and hsc filtered image with the edges marked with a red line
+    # show original image with the edges marked with a red line
     image_and_edges = deepcopy(image)
-    hsv_filtered_image_and_edges = deepcopy(hsv_filtered_image)
     for i, item_i in enumerate(edges_on_image):
         for j, item_j in enumerate(item_i):
             if item_j.all() != 0:
                 image_and_edges[i][j] = (0, 0, 255)
-                hsv_filtered_image_and_edges[i][j] = (0, 0, 255)
     cv.imshow("edges on original image", image_and_edges)
-    cv.imshow("edges on hsv filtered image", hsv_filtered_image_and_edges)
 
-    image_and_edges_hsv = deepcopy(image)
-    hsv_filtered_image_and_edges_hsv = deepcopy(hsv_filtered_image)
-    for i, item_i in enumerate(edges_after_hsv):
-        for j, item_j in enumerate(item_i):
-            if item_j.all() != 0:
-                image_and_edges_hsv[i][j] = (0, 0, 255)
-                hsv_filtered_image_and_edges_hsv[i][j] = (0, 0, 255)
-    cv.imshow("edges after hsv on original image", image_and_edges_hsv)
-    cv.imshow("edges after hsv on hsv filtered image", hsv_filtered_image_and_edges_hsv)
-    cv.imshow("edges bitwise", cv.bitwise_and(edges_on_image, edges_after_hsv))
+    # try color after edges
+    get_hsv_mask(image_and_edges, cv.cvtColor(image_and_edges, cv.COLOR_BGR2HSV))
 
     # try a segmentation approach
     # ret, thresh = cv.threshold(greyscale_image, 0, cv.ADAPTIVE_THRESH_GAUSSIAN_C, 11, 2)
